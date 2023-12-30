1e29aa5c7fef0e032988b2b106da11514cef678b
GetUpJohnnyBoi <johnhuntermitchell@gmail.com>
2023-12-29 18:05:25 -0600

    init commit

---
+++ ottergram/src/components/Post.js
@@ -0,0 +1,14 @@
+import Barry from '../otters/otter1.jpg';
+
+function Post({ image, name }) {
+    return (
+        <li className='post-component'>
+          <button>
+            <img src={image} alt={name}/>
+            <p className='post-name'>{name}</p>
+          </button>
+        </li>
+    )
+}
+
+export default Post;
