cca1bd861c20a931205d0090fe201e00d95f5173
GetUpJohnnyBoi <johnhuntermitchell@gmail.com>
2024-01-03 16:46:08 -0600

    set the onClick attribute equal to handleClick function

--- ottergram/src/components/Post.js
+++ ottergram/src/components/Post.js
@@ -1,14 +1,14 @@
 import Barry from '../otters/otter1.jpg';
 
 function Post({ image, name }) {
-  
+
   const handleClick = (event) => {
     return console.log(event)
   }
 
     return (
         <li className='post-component'>
-          <button>
+          <button onClick={handleClick}>
             <img src={image} alt={name}/>
             <p className='post-name'>{name}</p>
           </button>
