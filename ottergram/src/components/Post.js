c2a78ce162dfd951dbe891b6ec1eafc73e227d88
GetUpJohnnyBoi <johnhuntermitchell@gmail.com>
2024-01-03 18:06:20 -0600

    added onMouseEnter and onMouseLeave events for otters

--- ottergram/src/components/Post.js
+++ ottergram/src/components/Post.js
@@ -6,9 +6,19 @@ function Post({ image, name }) {
     return console.log('You clicked ' + otterName)
   }
 
+  const handleMouseEnter = (otterName) => {
+    return console.log('Hello, ' + otterName + '!')
+  }
+
+  const handleMouseLeave = (otterName) => {
+    return console.log('Goodbye, ' + otterName + '!')
+  }
+
     return (
         <li className='post-component'>
-          <button onClick={() => handleClick(name)}>
+          <button onClick={() => handleClick(name)}
+                  onMouseEnter={() => handleMouseEnter(name)}
+                  onMouseLeave={() => handleMouseLeave(name)}>
             <img src={image} alt={name}/>
             <p className='post-name'>{name}</p>
           </button>
