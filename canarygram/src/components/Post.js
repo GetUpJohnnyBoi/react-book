eb7ad3e1bcba9b5f59da560c87702e53a615a48d
GetUpJohnnyBoi <johnhuntermitchell@gmail.com>
2024-01-15 15:20:50 -0600

    updated to provide window.alert w/ canary name on user click

--- canarygram/src/components/Post.js
+++ canarygram/src/components/Post.js
@@ -2,13 +2,13 @@ import Lacey from '../canaries/img1.jpg';
 
 const Post = ({image, name}) => {
 
-    const handleClick = (event) => {
-        return console.log(event)
+    const handleClick = (canaryName) => {
+        return window.alert('You clicked ' + canaryName + '!')
     }
-    
+
     return (
         <li className='post-component'>
-            <button>
+            <button onClick={() => handleClick(name)}>
                 <img src={image} alt={name}/>
                 <p className='post-name'>{name}</p>
             </button>
