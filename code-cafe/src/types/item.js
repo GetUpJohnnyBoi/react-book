2881ff0cc2056ba13389543a58dd91dc15428df2
GetUpJohnnyBoi <johnhuntermitchell@gmail.com>
2024-01-23 12:47:33 -0600

    fixed issue with exporting the item prop type

--- code-cafe/src/types/item.js
+++ code-cafe/src/types/item.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 
 const ItemType = PropTypes.shape({
-    itemId: PropTypes.string.isRequired,
-    imageId: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    description: PropTypes.string,
-    salePrice: PropTypes.number,
-  }),
+  itemId: PropTypes.string.isRequired,
+  imageId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  description: PropTypes.string,
+  salePrice: PropTypes.number,
+});
 
-  export default ItemType;
+export default ItemType;
