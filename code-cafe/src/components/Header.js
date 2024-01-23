2f43ec11939cff48466efb8e0a6d4f4cb0f3ef83
GetUpJohnnyBoi <johnhuntermitchell@gmail.com>
2024-01-23 13:49:34 -0600

    added title prop

--- code-cafe/src/components/Header.js
+++ code-cafe/src/components/Header.js
@@ -1,13 +1,20 @@
+import PropTypes from 'prop-types';
 import CoffeeLogo from '../images/logo.svg';
 import './Header.css';
 
-function Header() {
+function Header({ title }) {
   return (
     <header className="header-component">
       <img src={CoffeeLogo} alt="coffee-logo" />
       <h1>Bed and Second Breakfast Café</h1>
+      &nbsp;
+      <h2>{title}</h2>
     </header>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
