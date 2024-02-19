38d7e240eddc068fc41167efe18367b4544d24f4
GetUpJohnnyBoi <johnhuntermitchell@gmail.com>
2024-02-19 13:42:17 -0600

    added a catchall route

--- code-cafe/src/App.js
+++ code-cafe/src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {
   BrowserRouter as Router,
+  Routes,
+  Route,
 } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -17,8 +19,11 @@ function App() {
 
   return (
     <Router className="App">
-      <Header title="Southaven" />
-      <Home items={items} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home items={items} />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
     </Router>
   );
 }
