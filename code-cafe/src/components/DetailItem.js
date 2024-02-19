4a0166d7956db508febdcd35ddcbd20aa2c5a5ff
GetUpJohnnyBoi <johnhuntermitchell@gmail.com>
2024-02-19 14:20:54 -0600

    used the useParams hook

--- code-cafe/src/components/DetailItem.js
+++ code-cafe/src/components/DetailItem.js
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { useParams } from 'react-router-dom';
 import './DetailItem.css';
 
 function DetailItem() {
+  const { id } = useParams();
   return (
     <div className="detail-item-component">
-      Detail Item
+      {id}
     </div>
   );
 }
